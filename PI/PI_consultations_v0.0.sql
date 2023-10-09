973aa9f1b81d52af2c12450e79ff7dfe2d4fc1a2
Kris <54648029+kris-reinhardt@users.noreply.github.com>
2023-10-09 11:42:32 +0200

    Adding address columns

--- PI/PI_consultations_v0.0.sql
+++ PI/PI_consultations_v0.0.sql
@@ -72,6 +72,9 @@ SELECT
 		ELSE NULL
 	END AS age_group_current,
 	pdd.gender,
+	pad.state_province AS governorate, 
+	pad.city_village AS community_village,
+	pad.address2 AS area,
 	cc.date::date AS visit_date,
 	cc.visit_location,
 	cc.intervention_setting,
@@ -85,4 +88,6 @@ LEFT OUTER JOIN entry_exit_cte eec
 LEFT OUTER JOIN patient_identifier pi
 	ON cc.patient_id = pi.patient_id
 LEFT OUTER JOIN person_details_default pdd 
-	ON cc.patient_id = pdd.person_id;
+	ON cc.patient_id = pdd.person_id
+LEFT OUTER JOIN person_address_default pad
+	ON eec.patient_id = pad.person_id;
