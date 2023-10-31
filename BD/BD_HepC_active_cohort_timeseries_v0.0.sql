b13108a365204a0d6774baca87b3a106f2afe1f4
Kris <54648029+kris-reinhardt@users.noreply.github.com>
2023-10-31 10:26:46 +0100

    Create BD_HepC_active_cohort_timeseries_v0.0.sql

---
+++ BD/BD_HepC_active_cohort_timeseries_v0.0.sql
@@ -0,0 +1,61 @@
+-- The first CTE build the frame for patients entering and exiting the cohort. This frame is based on NCD forms with visit types of 'initial visit' and 'discharge visit'. The query takes all initial visit dates and matches discharge visit dates if the discharge visit date falls between the initial visit date and the next initial visit date (if present).
+WITH initial AS (
+	SELECT 
+		patient_id, encounter_id AS initial_encounter_id, date AS initial_visit_date, DENSE_RANK () OVER (PARTITION BY patient_id ORDER BY date) AS initial_visit_order, LEAD (date) OVER (PARTITION BY patient_id ORDER BY date) AS next_initial_visit_date
+	FROM hepatitis_c WHERE visit_type = 'Initial visit'),
+cohort AS (
+	SELECT
+		i.patient_id, i.initial_encounter_id, i.initial_visit_date, d.date AS discharge_date
+	FROM initial i
+	LEFT JOIN (SELECT patient_id, date FROM hepatitis_c WHERE visit_type = 'Discharge visit') d 
+		ON i.patient_id = d.patient_id AND d.date >= i.initial_visit_date AND (d.date < i.next_initial_visit_date OR i.next_initial_visit_date IS NULL)),
+-- The following CTEs create the active patient calculation by counting each day the cumulative admissions and exits from the cohort.
+range_values AS (
+	SELECT 
+		date_trunc('day',min(c.initial_visit_date)) AS minval,
+		date_trunc('day',max(CURRENT_DATE)) AS maxval
+	FROM cohort c),
+day_range AS (
+	SELECT 
+		generate_series(minval,maxval,'1 day'::interval) as day
+	FROM range_values),   
+daily_admissions_total AS (
+	SELECT 
+		date_trunc('day', c.initial_visit_date) AS day,
+		count(*) AS patients
+	FROM cohort c
+	GROUP BY 1),
+daily_exits_total AS (
+	SELECT
+		date_trunc('day',c.discharge_date) AS day,
+		count(*) AS patients
+	FROM cohort c
+	GROUP BY 1),
+daily_active_patients AS (
+	SELECT 
+		dr.day as reporting_day,
+		sum(dat.patients) over (order by dr.day asc rows between unbounded preceding and current row) AS cumulative_admissions_total,
+		CASE
+		    WHEN sum(det.patients) over (order by dr.day asc rows between unbounded preceding and current row) IS NULL
+		    THEN 0
+		    ELSE sum(det.patients) over (order by dr.day asc rows between unbounded preceding and current row) 
+		END AS cumulative_exits_total, 
+		CASE
+		    WHEN sum(det.patients) over (order by dr.day asc rows between unbounded preceding and current row) IS NULL 
+			THEN sum(dat.patients) over (order by dr.day asc rows between unbounded preceding and current row)
+		    ELSE (sum(dat.patients) over (order by dr.day asc rows between unbounded preceding and current row)-
+				sum(det.patients) over (order by dr.day asc rows between unbounded preceding and current row)) 
+		END AS active_patients_total,
+		CASE 
+			WHEN date(dr.day)::date = (date_trunc('MONTH', dr.day) + INTERVAL '1 MONTH - 1 day')::date THEN 1
+		END AS last_day_of_month
+	FROM day_range dr
+	LEFT OUTER JOIN daily_admissions_total dat ON dr.day = dat.day
+	LEFT OUTER JOIN daily_exits_total det ON dr.day = det.day)
+-- Main query --
+SELECT 
+	dap.reporting_day,
+	dap.active_patients_total
+FROM daily_active_patients dap
+WHERE dap.last_day_of_month = 1 and dap.reporting_day > date_trunc('MONTH', CURRENT_DATE) - INTERVAL '1 year'
+GROUP BY dap.reporting_day, dap.active_patients_total;
