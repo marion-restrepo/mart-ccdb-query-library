9fcce241775435b72e1f63078aa71c3dbf207213
Kris <54648029+kris-reinhardt@users.noreply.github.com>
2024-07-05 14:51:38 +0200

    Update CAR_active_cohort_timeseries_v0.0.sql

--- CAR/CAR_active_cohort_timeseries_v0.0.sql
+++ CAR/CAR_active_cohort_timeseries_v0.0.sql
@@ -1,4 +1,4 @@
--- The first CTE build the frame for patients entering and exiting the cohort. This frame is based on MNT VIH TB form with visit types of 'Inclusion' and 'Sortie'. The query takes all initial visit dates and matches discharge visit dates if the discharge visit date falls between the initial visit date and the next initial visit date (if present).
+-- The first CTE build the frame for patients entering and exiting the cohort. This frame is based on MNT VIH TB form with visit types of 'Inclusion' and 'Sortie'. For each patient, the query takes all initial visit dates and matches the discharge visit date occuring after the initial visit. If a patient has multiple initial visits and discharge visits, the match happens sequentially based on the date of visit (e.g. the first initial visit is matched to the first discharge, and so on). If the patient does not have a discharge visit, then the discharge information is empty until completed. 
 WITH inclusion AS (
 	SELECT 
 		patient_id, encounter_id AS encounter_id_inclusion, lieu_de_visite AS lieu_de_visite_inclusion, date AS date_inclusion, DENSE_RANK () OVER (PARTITION BY patient_id ORDER BY date) AS inclusion_visit_order, LEAD (date) OVER (PARTITION BY patient_id ORDER BY date) AS date_inclusion_suivi
@@ -9,42 +9,30 @@ cohorte AS (
 	FROM inclusion i
 	LEFT JOIN (SELECT patient_id, date, encounter_id, date_de_sortie, statut_de_sortie FROM mnt_vih_tb WHERE type_de_visite = 'Sortie') d 
 		ON i.patient_id = d.patient_id AND d.date >= i.date_inclusion AND (d.date < i.date_inclusion_suivi OR i.date_inclusion_suivi IS NULL)),
--- The diagnosis CTEs select the last reported NCD diagnosis per cohort enrollment and pivots the data horizontally.
+-- The diagnosis CTE selects all reported diagnoses per cohort enrollment, both listing and pivoting the data horizonally. The pivoted diagnosis data is presented with the date the diagnosis was first reported.
 diagnostic_cohorte AS (
 	SELECT
-		d.patient_id, c.encounter_id_inclusion, n.date, d.diagnostic
-	FROM diagnostic d 
+		DISTINCT ON (d.patient_id, c.encounter_id_inclusion, d.diagnostic_group) d.patient_id, c.encounter_id_inclusion, CASE WHEN d.diagnostic_group = 'mnt' THEN n.date ELSE NULL END AS mnt, CASE WHEN d.diagnostic_group = 'vih' THEN n.date ELSE NULL END AS vih, CASE WHEN d.diagnostic_group = 'tb' THEN n.date ELSE NULL END AS tb, CASE WHEN d.diagnostic_group = 'santé_mentale' THEN n.date ELSE NULL END AS santé_mentale
+	FROM (SELECT 
+			patient_id, 
+			encounter_id, 
+			CASE WHEN diagnostic IN ('Asthme','Drépanocytose','Insuffisance renale chronique','Maladie cardiovasculaire','Bronchopneumopathie chronique obstructive','Diabète sucré de type 1','Diabète sucré de type 2','Hypertension','Hypothyroïdie','Hyperthyroïdie','Épilepsie focale','Épilepsie généralisée','Épilepsie non classifiée','Autre') THEN 'mnt' WHEN diagnostic IN ('Tuberculose pulmonaire','Tuberculose extrapulmonaire') THEN 'tb' WHEN diagnostic = 'Infection par le VIH' THEN 'vih' WHEN diagnostic = 'Troubles de santé mentale' THEN 'santé_mentale'	ELSE NULL END AS diagnostic_group 
+		FROM diagnostic) d 
 	LEFT JOIN mnt_vih_tb n USING(encounter_id)
-	LEFT JOIN cohorte c ON d.patient_id = c.patient_id AND c.date_inclusion <= n.date AND CASE WHEN c.date_de_sortie IS NOT NULL THEN c.date_de_sortie ELSE current_date END >= n.date),
-dernière_diagnostic_cohorte AS (
-	SELECT cd.patient_id, cd.encounter_id_inclusion, cd.date, cd.diagnostic
-	FROM diagnostic_cohorte cd
-	INNER JOIN (SELECT encounter_id_inclusion, MAX(date) AS max_date FROM diagnostic_cohorte GROUP BY encounter_id_inclusion) cd2 ON cd.encounter_id_inclusion = cd2.encounter_id_inclusion AND cd.date = cd2.max_date),
-dernière_diagnostic_cohorte_pivot AS (
-	SELECT 
-		DISTINCT ON (encounter_id_inclusion, patient_id, date) encounter_id_inclusion, 
-		patient_id, 
-		date,
-		MAX (CASE WHEN diagnostic IN ('Asthme','Drépanocytose','Insuffisance renale chronique','Maladie cardiovasculaire','Bronchopneumopathie chronique obstructive','Diabète sucré de type 1','Diabète sucré de type 2','Hypertension','Hypothyroïdie','Hyperthyroïdie','Épilepsie focale','Épilepsie généralisée','Épilepsie non classifiée','Autre') THEN 1 ELSE NULL END) AS mnt,
-		MAX (CASE WHEN diagnostic IN ('Tuberculose pulmonaire','Tuberculose extrapulmonaire') THEN 1 ELSE NULL END) AS tb,
-		MAX (CASE WHEN diagnostic = 'Infection par le VIH' THEN 1 ELSE NULL END) AS vih,
-		MAX (CASE WHEN diagnostic = 'Troubles de santé mentale' THEN 1 ELSE NULL END) AS santé_mentale	
-	FROM dernière_diagnostic_cohorte
-	GROUP BY encounter_id_inclusion, patient_id, date),
+	LEFT JOIN cohorte c ON d.patient_id = c.patient_id AND c.date_inclusion <= n.date AND CASE WHEN c.date_de_sortie IS NOT NULL THEN c.date_de_sortie ELSE current_date END >= n.date
+	ORDER BY d.patient_id, c.encounter_id_inclusion, d.diagnostic_group, n.date),
 -- The following CTEs create the active patient calculation by counting each day the cumulative admissions and exits from the cohort.
 cohorte_active AS (
 	SELECT
 		c.date_inclusion, 
 		c.date_de_sortie,
-		CASE 
-			WHEN ddcp.mnt = 1 AND ddcp.tb IS NULL AND ddcp.vih IS NULL AND ddcp.santé_mentale IS NULL THEN 'mnt'
-			WHEN ddcp.mnt IS NULL AND ddcp.tb = 1 AND ddcp.vih IS NULL AND ddcp.santé_mentale IS NULL THEN 'tb'
-			WHEN ddcp.mnt IS NULL AND ddcp.tb IS NULL AND ddcp.vih = 1 AND ddcp.santé_mentale IS NULL THEN 'vih'
-			WHEN ddcp.mnt IS NULL AND ddcp.tb IS NULL AND ddcp.vih IS NULL AND ddcp.santé_mentale = 1 THEN 'santé_mentale'
-			WHEN ddcp.mnt = 1 AND ddcp.tb IS NULL AND ddcp.vih = 1 AND ddcp.santé_mentale IS NULL THEN 'mnt_vih' ELSE NULL END AS cohorte
+		dc.mnt, 
+		dc.vih, 
+		dc.tb, 
+		dc.santé_mentale
 	FROM cohorte c
-	LEFT OUTER JOIN dernière_diagnostic_cohorte_pivot ddcp
-		ON c.encounter_id_inclusion = ddcp.encounter_id_inclusion),
+	LEFT OUTER JOIN diagnostic_cohorte dc
+		ON c.encounter_id_inclusion = dc.encounter_id_inclusion),
 range_values AS (
 	SELECT 
 		date_trunc('day',min(ca.date_inclusion)) AS minval,
@@ -62,38 +50,31 @@ daily_admissions AS (
 	GROUP BY 1),
 daily_admissions_mnt AS (
 	SELECT 
-		date_trunc('day', ca.date_inclusion) AS day,
+		date_trunc('day', ca.mnt) AS day,
 		count(*) AS patients
 	FROM cohorte_active ca
-	WHERE cohorte = 'mnt'
+	WHERE mnt IS NOT NULL
 	GROUP BY 1),
 daily_admissions_vih AS (
 	SELECT 
-		date_trunc('day', ca.date_inclusion) AS day,
+		date_trunc('day', ca.vih) AS day,
 		count(*) AS patients
 	FROM cohorte_active ca
-	WHERE cohorte = 'vih'
+	WHERE vih IS NOT NULL
 	GROUP BY 1),
 daily_admissions_tb AS (
 	SELECT 
-		date_trunc('day', ca.date_inclusion) AS day,
+		date_trunc('day', ca.tb) AS day,
 		count(*) AS patients
 	FROM cohorte_active ca
-	WHERE cohorte = 'tb'
+	WHERE tb IS NOT NULL
 	GROUP BY 1),
 daily_admissions_sm AS (
 	SELECT 
-		date_trunc('day', ca.date_inclusion) AS day,
+		date_trunc('day', ca.santé_mentale) AS day,
 		count(*) AS patients
 	FROM cohorte_active ca
-	WHERE cohorte = 'santé_mentale'
-	GROUP BY 1),
-daily_admissions_mnt_vih AS (
-	SELECT 
-		date_trunc('day', ca.date_inclusion) AS day,
-		count(*) AS patients
-	FROM cohorte_active ca
-	WHERE cohorte = 'mnt_vih'
+	WHERE santé_mentale IS NOT NULL
 	GROUP BY 1),
 daily_exits AS (
 	SELECT
@@ -106,35 +87,28 @@ daily_exits_mnt AS (
 		date_trunc('day',ca.date_de_sortie) AS day,
 		count(*) AS patients
 	FROM cohorte_active ca
-	WHERE cohorte = 'mnt'
+	WHERE MNT IS NOT NULL
 	GROUP BY 1),
 daily_exits_vih AS (
 	SELECT
 		date_trunc('day',ca.date_de_sortie) AS day,
 		count(*) AS patients
 	FROM cohorte_active ca
-	WHERE cohorte = 'vih'
+	WHERE vih IS NOT NULL
 	GROUP BY 1),
 daily_exits_tb AS (
 	SELECT
 		date_trunc('day',ca.date_de_sortie) AS day,
 		count(*) AS patients
 	FROM cohorte_active ca
-	WHERE cohorte = 'tb'
+	WHERE tb IS NOT NULL
 	GROUP BY 1),
 daily_exits_sm AS (
 	SELECT
 		date_trunc('day',ca.date_de_sortie) AS day,
 		count(*) AS patients
 	FROM cohorte_active ca
-	WHERE cohorte = 'santé_mentale'
-	GROUP BY 1),
-daily_exits_mnt_vih AS (
-	SELECT
-		date_trunc('day',ca.date_de_sortie) AS day,
-		count(*) AS patients
-	FROM cohorte_active ca
-	WHERE cohorte = 'mnt_vih'
+	WHERE santé_mentale IS NOT NULL
 	GROUP BY 1),
 daily_cohorte_active AS (
 	SELECT 
@@ -198,19 +172,7 @@ daily_cohorte_active AS (
 			THEN SUM(das.patients) over (order by dr.day asc rows between unbounded preceding and current row)
 		    ELSE (SUM(das.patients) over (order by dr.day asc rows between unbounded preceding and current row)-
 				SUM(des.patients) over (order by dr.day asc rows between unbounded preceding and current row)) 
-		END AS cohorte_active_sm,
-		SUM(damv.patients) over (order by dr.day asc rows between unbounded preceding and current row) AS cumulative_admissions_mnt_vih,
-		CASE
-		    WHEN SUM(demv.patients) over (order by dr.day asc rows between unbounded preceding and current row) IS NULL
-		    THEN 0
-		    ELSE SUM(demv.patients) over (order by dr.day asc rows between unbounded preceding and current row) 
-		END AS cumulative_exits_mnt_vih,  
-		CASE
-		    WHEN SUM(demv.patients) over (order by dr.day asc rows between unbounded preceding and current row) IS NULL 
-			THEN SUM(damv.patients) over (order by dr.day asc rows between unbounded preceding and current row)
-		    ELSE (SUM(damv.patients) over (order by dr.day asc rows between unbounded preceding and current row)-
-				SUM(demv.patients) over (order by dr.day asc rows between unbounded preceding and current row)) 
-		END AS cohorte_active_mnt_vih,		
+		END AS cohorte_active_sm,	
 		CASE 
 			WHEN date(dr.day)::date = (date_trunc('MONTH', dr.day) + INTERVAL '1 MONTH - 1 day')::date THEN 1
 		END AS last_day_of_month
@@ -220,13 +182,11 @@ daily_cohorte_active AS (
 	LEFT OUTER JOIN daily_admissions_vih dav ON dr.day = dav.day
 	LEFT OUTER JOIN daily_admissions_tb dat ON dr.day = dat.day
 	LEFT OUTER JOIN daily_admissions_sm das ON dr.day = das.day
-	LEFT OUTER JOIN daily_admissions_mnt_vih damv ON dr.day = damv.day
 	LEFT OUTER JOIN daily_exits de ON dr.day = de.day
 	LEFT OUTER JOIN daily_exits_mnt dem ON dr.day = dem.day
 	LEFT OUTER JOIN daily_exits_vih dev ON dr.day = dev.day
 	LEFT OUTER JOIN daily_exits_tb det ON dr.day = det.day
-	LEFT OUTER JOIN daily_exits_sm des ON dr.day = des.day
-	LEFT OUTER JOIN daily_exits_mnt_vih demv ON dr.day = demv.day)
+	LEFT OUTER JOIN daily_exits_sm des ON dr.day = des.day)
 -- Main query --
 SELECT 
 	dap.reporting_day::date,
@@ -234,8 +194,7 @@ SELECT
 	dap.cohorte_active_mnt AS mnt,
 	dap.cohorte_active_vih AS vih,
 	dap.cohorte_active_tb AS tb,
-	dap.cohorte_active_sm AS santé_mentale,
-	dap.cohorte_active_mnt_vih AS mnt_vih
+	dap.cohorte_active_sm AS santé_mentale
 FROM daily_cohorte_active dap
 WHERE dap.last_day_of_month = 1 and dap.reporting_day > date_trunc('MONTH', CURRENT_DATE) - INTERVAL '1 year'
-GROUP BY dap.reporting_day, dap.cohorte_active, dap.cohorte_active_mnt, dap.cohorte_active_vih, dap.cohorte_active_tb, dap.cohorte_active_sm, dap.cohorte_active_mnt_vih;
+GROUP BY dap.reporting_day, dap.cohorte_active, dap.cohorte_active_mnt, dap.cohorte_active_vih, dap.cohorte_active_tb, dap.cohorte_active_sm;
