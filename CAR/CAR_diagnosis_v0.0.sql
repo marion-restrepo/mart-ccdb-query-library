3a36867cb4f884d55be2744d4bc699f1611dceb0
Kris <54648029+kris-reinhardt@users.noreply.github.com>
2024-07-05 14:12:23 +0200

    Update CAR_diagnosis_v0.0.sql

--- CAR/CAR_diagnosis_v0.0.sql
+++ CAR/CAR_diagnosis_v0.0.sql
@@ -1,4 +1,4 @@
--- The first CTE build the frame for patients entering and exiting the cohort. This frame is based on MNT VIH TB form with visit types of 'Inclusion' and 'Sortie'. The query takes all initial visit dates and matches discharge visit dates if the discharge visit date falls between the initial visit date and the next initial visit date (if present).
+-- The first CTE build the frame for patients entering and exiting the cohort. This frame is based on MNT VIH TB form with visit types of 'Inclusion' and 'Sortie'. For each patient, the query takes all initial visit dates and matches the discharge visit date occuring after the initial visit. If a patient has multiple initial visits and discharge visits, the match happens sequentially based on the date of visit (e.g. the first initial visit is matched to the first discharge, and so on). If the patient does not have a discharge visit, then the discharge information is empty until completed. 
 WITH inclusion AS (
 	SELECT 
 		patient_id, encounter_id AS encounter_id_inclusion, lieu_de_visite AS lieu_de_visite_inclusion, date AS date_inclusion, DENSE_RANK () OVER (PARTITION BY patient_id ORDER BY date) AS inclusion_visit_order, LEAD (date) OVER (PARTITION BY patient_id ORDER BY date) AS date_inclusion_suivi
@@ -9,29 +9,26 @@ cohorte AS (
 	FROM inclusion i
 	LEFT JOIN (SELECT patient_id, date, encounter_id, date_de_sortie, statut_de_sortie FROM mnt_vih_tb WHERE type_de_visite = 'Sortie') d 
 		ON i.patient_id = d.patient_id AND d.date >= i.date_inclusion AND (d.date < i.date_inclusion_suivi OR i.date_inclusion_suivi IS NULL)),
--- The diagnosis CTEs select the last reported NCD diagnosis per cohort enrollment and pivots the data horizontally.
+-- The diagnosis CTE selects all reported diagnoses per cohort enrollment, both listing and pivoting the data horizonally. The pivoted diagnosis data is presented with the date the diagnosis was first reported.
 diagnostic_cohorte AS (
 	SELECT
-		d.patient_id, c.encounter_id_inclusion, n.date, d.diagnostic
+		DISTINCT ON (d.patient_id, d.diagnostic) d.patient_id, c.encounter_id_inclusion, n.date, d.diagnostic
 	FROM diagnostic d 
 	LEFT JOIN mnt_vih_tb n USING(encounter_id)
-	LEFT JOIN cohorte c ON d.patient_id = c.patient_id AND c.date_inclusion <= n.date AND CASE WHEN c.date_de_sortie IS NOT NULL THEN c.date_de_sortie ELSE current_date END >= n.date),
-dernière_diagnostic_cohorte AS (
-	SELECT cd.patient_id, cd.encounter_id_inclusion, cd.date, cd.diagnostic
-	FROM diagnostic_cohorte cd
-	INNER JOIN (SELECT encounter_id_inclusion, MAX(date) AS max_date FROM diagnostic_cohorte GROUP BY encounter_id_inclusion) cd2 ON cd.encounter_id_inclusion = cd2.encounter_id_inclusion AND cd.date = cd2.max_date),
--- The last visit location CTE finds the last visit location reported in clinical forms.
+	LEFT JOIN cohorte c ON d.patient_id = c.patient_id AND c.date_inclusion <= n.date AND CASE WHEN c.date_de_sortie IS NOT NULL THEN c.date_de_sortie ELSE current_date END >= n.date
+	ORDER BY d.patient_id, d.diagnostic, n.date),
+-- The last visit location CTE finds the last visit location reported in clinical forms (including MNT VIH TB, PTPE).
 dernière_fiche_location AS (	
 	SELECT 
 		DISTINCT ON (c.patient_id, c.encounter_id_inclusion, c.date_inclusion, c.date_de_sortie) c.encounter_id_inclusion,
-		nvsl.lieu_de_visite AS dernière_fiche_location
+		forms.lieu_de_visite AS dernière_fiche_location
 	FROM cohorte c
 	LEFT OUTER JOIN (SELECT patient_id, CASE WHEN type_de_visite = 'Sortie' AND date_de_sortie IS NOT NULL THEN date_de_sortie ELSE date END AS date, lieu_de_visite FROM mnt_vih_tb UNION 
-	SELECT patient_id, date, lieu_de_visite FROM ptpe) nvsl
-		ON c.patient_id = nvsl.patient_id AND c.date_inclusion <= nvsl.date::date AND CASE WHEN c.date_de_sortie IS NOT NULL THEN c.date_de_sortie ELSE current_date END >= nvsl.date::date
-	WHERE nvsl.lieu_de_visite IS NOT NULL
-	GROUP BY c.patient_id, c.encounter_id_inclusion, c.date_inclusion, c.date_de_sortie, nvsl.date, nvsl.lieu_de_visite
-	ORDER BY c.patient_id, c.encounter_id_inclusion, c.date_inclusion, c.date_de_sortie, nvsl.date DESC)
+	SELECT patient_id, date, lieu_de_visite FROM ptpe) forms
+		ON c.patient_id = forms.patient_id AND c.date_inclusion <= forms.date::date AND CASE WHEN c.date_de_sortie IS NOT NULL THEN c.date_de_sortie ELSE current_date END >= forms.date::date
+	WHERE forms.lieu_de_visite IS NOT NULL
+	GROUP BY c.patient_id, c.encounter_id_inclusion, c.date_inclusion, c.date_de_sortie, forms.date, forms.lieu_de_visite
+	ORDER BY c.patient_id, c.encounter_id_inclusion, c.date_inclusion, c.date_de_sortie, forms.date DESC)
 -- Main query -- 
 SELECT 
 	pi."Patient_Identifier",
@@ -50,16 +47,16 @@ SELECT
 		WHEN pdd.age::int >= 65 THEN '65+'
 		ELSE NULL
 	END AS groupe_age_actuel,
-	EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) AS age_inclusion,
+	EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) AS age_inclusion,
 	CASE 
-		WHEN EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int <= 4 THEN '0-4'
-		WHEN EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 5 AND EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 14 THEN '05-14'
-		WHEN EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 15 AND EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 24 THEN '15-24'
-		WHEN EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 25 AND EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 34 THEN '25-34'
-		WHEN EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 35 AND EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 44 THEN '35-44'
-		WHEN EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 45 AND EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 54 THEN '45-54'
-		WHEN EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 55 AND EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 64 THEN '55-64'
-		WHEN EXTRACT(YEAR FROM (SELECT age(ped.encounter_datetime, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 65 THEN '65+'
+		WHEN EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int <= 4 THEN '0-4'
+		WHEN EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 5 AND EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 14 THEN '05-14'
+		WHEN EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 15 AND EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 24 THEN '15-24'
+		WHEN EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 25 AND EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 34 THEN '25-34'
+		WHEN EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 35 AND EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 44 THEN '35-44'
+		WHEN EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 45 AND EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 54 THEN '45-54'
+		WHEN EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 55 AND EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy')))) <= 64 THEN '55-64'
+		WHEN EXTRACT(YEAR FROM (SELECT age(c.date_inclusion, TO_DATE(CONCAT('01-01-', pdd.birthyear), 'dd-MM-yyyy'))))::int >= 65 THEN '65+'
 		ELSE NULL
 	END AS groupe_age_inclusion,
 	pdd.gender AS sexe,
@@ -67,20 +64,18 @@ SELECT
 	CASE WHEN c.date_de_sortie IS NULL THEN 'Oui' END AS en_cohorte,
 	c.readmission,
 	c.lieu_de_visite_inclusion,
-	lvl.dernière_fiche_location,	
+	lfl.dernière_fiche_location,	
 	c.date_de_sortie,
 	c.statut_de_sortie,
-	lcd.diagnostic
-FROM dernière_diagnostic_cohorte lcd
+	dc.diagnostic
+FROM diagnostic_cohorte dc
 LEFT OUTER JOIN cohorte c
-	ON lcd.encounter_id_inclusion = c.encounter_id_inclusion
+	ON dc.encounter_id_inclusion = c.encounter_id_inclusion
 LEFT OUTER JOIN patient_identifier pi
 	ON c.patient_id = pi.patient_id
 LEFT OUTER JOIN person_attributes pa
 	ON c.patient_id = pa.person_id
 LEFT OUTER JOIN person_details_default pdd 
 	ON c.patient_id = pdd.person_id
-LEFT OUTER JOIN patient_encounter_details_default ped 
-	ON c.encounter_id_inclusion = ped.encounter_id
-LEFT OUTER JOIN dernière_fiche_location lvl
-	ON c.encounter_id_inclusion = lvl.encounter_id_inclusion;
+LEFT OUTER JOIN dernière_fiche_location lfl
+	ON c.encounter_id_inclusion = lfl.encounter_id_inclusion;
