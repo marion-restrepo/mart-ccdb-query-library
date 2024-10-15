516944bb44a66837e8d796092ee21a65a920cfd0
Kris <54648029+kris-reinhardt@users.noreply.github.com>
2024-10-15 14:57:00 +0200

    adding sample code for CBO

--- MW/CBO_register.sql
+++ MW/CBO_register.sql
@@ -1,3 +1,11 @@
 -- This is a test file for the CBO register. 
 -- adding a new line of code.
 -- add some more code here as a demo
+WITH sub-table1 AS (),
+sub-table2 AS ()
+-- Main query --
+SELECT
+*
+FROM patient_identifier
+LEFT OUTER JOIN sub-table1 st1 USING(patient_id)
+LEFT OUTER JOIN sub-table2 st2 USING(patient_id)
